dd79bb3bc293b923561a9995af364e8cddd6ee57
NNNag <41707030+NNNag@users.noreply.github.com>
2021-04-27 23:27:51 -0400

    Update users.js

--- test/users.js
+++ test/users.js
@@ -30,7 +30,7 @@ describe('Step-2', ()  => {
     });
 });
 
-describe('Step-4', ()  => {
+describe('Step-3', ()  => {
     it('fetch the standings of teams for the season 2015', () => {
             const url = 'v1/standings/2015?key=T8Mv9BRDdcB7bMQUsQvHqtCGPewH5y8p';
             return request.get(url).then((res) => {
@@ -40,7 +40,7 @@ describe('Step-4', ()  => {
      });
 });  
 
-describe('Step-6', ()  => {
+describe('Step-4', ()  => {
     it('fetch the list of games for the season 2015', () => {
         const url = 'v1/games/2015?key=T8Mv9BRDdcB7bMQUsQvHqtCGPewH5y8p';
         return request.get(url). then((res) => {
@@ -49,3 +49,34 @@ describe('Step-6', ()  => {
         });
     });
 });
+
+describe('Step-5', ()  => {
+    it('fetch all the Teams', () => {
+            const url = 'v1/teams?key=T8Mv9BRDdcB7bMQUsQvHqtCGPewH5y8p';
+            return request.get(url).then((res) => {
+                expect(res.body.data).to.not.be.empty;
+                console.log(res.body);
+            });
+     });
+}); 
+
+describe('Step-6', ()  => {
+    it('fetch all the Seasons', () => {
+            const url = 'v1/seasons?key=T8Mv9BRDdcB7bMQUsQvHqtCGPewH5y8p';
+            return request.get(url).then((res) => {
+                expect(res.body.data).to.not.be.empty;
+                console.log(res.body);
+            });
+     });
+});
+
+describe('Step-7', ()  => {
+    it('fetch all the Team Leads', () => {
+            const url = 'v1.1/team_leaders/2018/offence/category/game_stats?key=T8Mv9BRDdcB7bMQUsQvHqtCGPewH5y8p';
+            return request.get(url).then((res) => {
+                expect(res.body.data).to.not.be.empty;
+                console.log(res.body);
+            });
+     });
+});
+
